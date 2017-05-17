9316773b1a5ced129d263900806b4c79b968814c
nlownnl <tylerhartman8@gmail.com>
2017-05-17 13:50:48 -0600

    Added JOP Welcoming Message

--- src/Runner.java
+++ src/Runner.java
@@ -1,10 +1,19 @@
+import javax.swing.JOptionPane;
 
 public class Runner {
 
 	public static void main(String[] args) {
 		
+		Runner myRunner = new Runner();
+		Converter myConverter = new Converter();
+		myRunner.welcome();
 		
 
+	}
+
+	private void welcome() {
+		JOptionPane.showMessageDialog(null, "Welecome to my Converter Program, created By Tyler H.");
+		
 	}
 
 }
